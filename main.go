4c51832435d6d022f75dba3ac06ad87959364419
Soheil Hoseini <soheil.hosseini78@gmail.com>
2023-05-03 13:52:24 +0430

    data types in golang

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
+
 import "fmt"
 
 func main(){
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets  = 45
-	var remainingTickets = 45
+	var remainingTickets int = 45
+	var platformName = "Aryans"
 
-	fmt.Println("Welcome to Aryans Booking Platform!")
+	fmt.Printf(" *** Welcome to %v Booking Platform! ***\n", platformName)
 	fmt.Println("Get your tickets here to attend " + conferenceName)
 	fmt.Println("All tickets:", conferenceTickets, "Remaining:", remainingTickets)
+
+	// print type of a variable using %T
+	fmt.Printf("remainigTickets type is: %T\n", conferenceName)
+
+	var userName string
+	var userTickets int
+	userName = "Shir Zard"
+
+	fmt.Printf("User %v booked %v tikcets.", userName, userTickets)
+
 }
