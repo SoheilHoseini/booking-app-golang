6efea330f9e7a8866839db07915e7b9df8b3cb3f
Soheil Hoseini <soheil.hosseini78@gmail.com>
2023-05-04 22:16:32 +0430

    Update main.go

--- main.go
+++ main.go
@@ -1,24 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main(){
 	conferenceName := "Go Conference"
 	const conferenceTickets  = 45
-	var remainingTickets int = 45
+	var remainingTickets int = 45 
 	var platformName = "Aryans"
+	var firstName string
+	var lastName string
+	var userTickets int
+	var bookings []string
+	bookings = append(bookings, "STGI", "Barbosa")
+	
 
+	remainingTickets -= userTickets
 	fmt.Printf(" *** Welcome to %v Booking Platform! ***\n", platformName)
 	fmt.Println("Get your tickets here to attend " + conferenceName)
-	fmt.Println("All tickets:", conferenceTickets, "Remaining:", remainingTickets)
+	fmt.Println("All tickets:", conferenceTickets, "Remaining:", remainingTickets, "\n")
 
-	// print type of a variable using %T
-	fmt.Printf("remainigTickets type is: %T\n", conferenceName)
+	for i := 0; i < 3; i++ {
+		fmt.Println("Enter your first name:")
+		fmt.Scan(&firstName)
+		fmt.Println("Enter your last name:")
+		fmt.Scan(&lastName)
+		fmt.Println("Enter number of tickets:")
+		fmt.Scan(&userTickets)
 
-	var userName string
-	var userTickets int
-	userName = "Shir Zard"
-
-	fmt.Printf("User %v booked %v tikcets.", userName, userTickets)
-
+		
+	}
+	
+	fmt.Printf("User %v %v booked %v tikcets.", firstName, lastName, userTickets)
 }
